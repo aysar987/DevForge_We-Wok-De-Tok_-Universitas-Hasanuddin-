3effb33428f3657478dc2d96e331d0f2aa57b088
aysar987 <aysrakbar2006@gmail.com>
2025-06-22 00:42:31 +0800

    commit

--- Reacting/PhishScan/src/App.tsx
+++ Reacting/PhishScan/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/Landing-Page';
 import PhishingInfoPage from './components/Info-page';
 import ExecutePage from './components/execute-page';
+import SafePage from './components/safe';
+import UnsafePage from './components/unsafe';
 import './App.css'
 
 function App() {
@@ -10,8 +12,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/info" element={<PhishingInfoPage />} />\
-         <Route path="/execute" element={<ExecutePage />} />
+        <Route path="/info" element={<PhishingInfoPage />} />
+        <Route path="/execute" element={<ExecutePage />} />
+        <Route path="/safe" element={<SafePage/>}/>
+        <Route path="/unsafe" element={<UnsafePage/>}/>
       </Routes>
     </Router>
   );
