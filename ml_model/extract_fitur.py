48f7f5aec43196e75824ae64858534b52cf7bd29
MiraiEst <cristikoesteranto@gmail.com>
2025-06-21 11:35:03 +0800

    update

--- ml_model/extract_fitur.py
+++ ml_model/extract_fitur.py
@@ -4,4 +4,11 @@ import ssl
 import math
 from urllib.parse import urlparse
 from datetime import datetime
+from whois_cache import load_whois_cache, save_whois_cache
 
+def subdomain_count(url):
+    parsed_url = urlparse(url)
+    domain_parts = parsed_url.netloc.split('.')
+    return len(domain_parts) - 2 if len(domain_parts) > 2 else 0
+
+def calculate
--- ml_model/whois_cache.py
+++ ml_model/whois_cache.py
@@ -1,3 +1,16 @@
 import json
 import os
 
+WHOIS_CACHE_FILE = 'whois_cache.json'
+
+def load_whois_cache():
+    """Load the WHOIS cache from a JSON file."""
+    if os.path.exists(WHOIS_CACHE_FILE):
+        with open(WHOIS_CACHE_FILE, 'r') as file:
+            return json.load(file)
+    return {}  
+
+def save_whois_cache(cache):
+    """Save the WHOIS cache to a JSON file."""
+    with open(WHOIS_CACHE_FILE, 'w') as file:
+        json.dump(cache, file, indent=4)
